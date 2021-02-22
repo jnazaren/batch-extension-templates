c821eb777878c6867980263178fa593b7346ac47
Jacob Nazarenko <jnazaren@gmail.com>
2021-02-22 00:50:19 -0500

    Add linux-cycles-gpu job/pool and update setup script

--- templates/blender/scripts/setup-linux-pool.sh
+++ templates/blender/scripts/setup-linux-pool.sh
@@ -4,11 +4,13 @@ apt-get update
 echo '# Installing imagemagick ...'
 apt-get install -y imagemagick
 echo '# Installing blender ...'
-apt-get install -y blender
+mkdir blender-download && cd blender-download
+wget https://download.blender.org/release/Blender2.79/blender-2.79b-linux-glibc219-x86_64.tar.bz2
+tar -xvf blender-2.79b-linux-glibc219-x86_64.tar.bz2
+export PATH=$PATH:`pwd`/blender-2.79b-linux-glibc219-x86_64
+echo $PATH
 echo '# Installing python-pip ...'
 apt-get install -y python-pip
-echo '# Installing azure-batch ...'
-pip install azure-batch==4.1.3
 echo '# Installing packages-microsoft-prod.deb ...'
 wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
